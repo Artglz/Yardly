391d6cf19aa375e7eea69f9c865cc93db489d900
Artglz <arturoglz2004@gmail.com>
2024-07-06 17:28:53 -0500

    changing submit-form

--- api/submit-form.js
+++ api/submit-form.js
@@ -1,34 +1,28 @@
 // api/submitForm.js
 const nodemailer = require('nodemailer');
 
-async function sendEmail(name, email) {
-    // Setup transporter using your email service credentials
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
 
-    // Setup email data
-    let mailOptions = {
-        from: process.env.EMAIL_USER,
-        to: process.env.EMAIL_USER,
-        subject: 'New Form Submission',
-        text: `Form submission received:\nName: ${name}\nEmail: ${email}\nAddress: ${address}`
-    };
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
-    // Send email
-    await transporter.sendMail(mailOptions);
-}
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { name, email } = req.body;
-        const address = req.body['Home Address'];;
+        const address = req.body['Home Address'];
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER, 
+            subject: 'New Form Submission',
+            text: `Form submission received:\nName: ${name}\nEmail: ${email}\nAddress: ${address}`
+        };
         try {
-            await sendEmail(name, email, address);
+            await transporter.sendEmail(mailOptions);
             res.status(200).json({ message: 'Email sent successfully' });
         } catch (error) {
             console.error('Error sending email:', error);
